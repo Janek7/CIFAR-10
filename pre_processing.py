876994284ec58b242329dfefcbd579af89c54c84
Janek <janekputz@web.de>
2020-03-26 22:39:17 +0100

    build and train model

--- pre_processing.py
+++ pre_processing.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from os import listdir
 from os.path import isfile, join
 import numpy as np
@@ -14,7 +15,12 @@ def transform_images(dir_path):
     :param dir_path: path of directory
     :return: images as four dimensional numpy array
     """
-    image_paths = [join(dir_path, file_path) for file_path in listdir(dir_path) if isfile(join(dir_path, file_path))]
+    image_paths = [join(dir_path, file_path) for file_path in listdir(dir_path)
+                   if isfile(join(dir_path, file_path)) and '.png' in file_path]
+    image_path_tuples = [(image_path, int(re.search('.*\\\\(.*)\.png', image_path).group(1)))
+                         for image_path in image_paths]
+    image_path_tuples.sort(key=lambda x: x[1])
+    image_paths = [image_path_tuple[0] for image_path_tuple in image_path_tuples]
     return np.asarray([np.asarray(Image.open(image_path), dtype='int32') for image_path in image_paths])
 
 
@@ -37,19 +43,19 @@ def load_train_labels():
     """
     config = get_config()
     with open(config['image_paths']['train_labels'], newline='') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=',',)
+        reader = csv.DictReader(csv_file, delimiter=',', )
         labels = np.asarray([row['label'] for row in reader])
         label_dict = get_label_dict()
         return np.asarray([label_dict[label_string] for label_string in labels])
 
 
 if __name__ == '__main__':
-    # config = get_config()
-    # train_images_np = transform_images(config['image_paths']['train_images_raw'])
-    # save_as_pickle(train_images_np, config['image_paths']['train_images_pickle'])
-    # test_images_np = transform_images(config['image_paths']['test_images_raw'])
-    # save_as_pickle(test_images_np, config['image_paths']['test_images_pickle'])
-    load_train_labels()
+    config = get_config()
+    train_images_np = transform_images(config['image_paths']['train_images_raw'])
+    save_as_pickle(train_images_np, config['image_paths']['train_images_pickle'])
+    test_images_np = transform_images(config['image_paths']['test_images_raw'])
+    save_as_pickle(test_images_np, config['image_paths']['test_images_pickle'])
+    # load_train_labels()
 
     # loaded = load_pickle( config['image_paths']['train_images_pickle'] )
     # print(loaded.shape)
